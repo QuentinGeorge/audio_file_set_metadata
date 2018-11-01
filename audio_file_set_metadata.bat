966bcfd6826b2868d95935e03ee6ce0a22b82a38
Quentin George <quentingeorge7@gmail.com>
2018-11-01 21:21:15 +0100

    Update batch file

--- audio_file_set_metadata.bat
+++ audio_file_set_metadata.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-set scriptPath=D:\Projects\audio_file_set_metadata\
-set scriptName=audio_file_set_metadata.php
+set scriptPath=D:\Projects\audio_file_set_metadata
+set scriptName=\audio_file_set_metadata.php
 php %scriptPath%%scriptName% -d "%cd%"
 ECHO.
 PAUSE
